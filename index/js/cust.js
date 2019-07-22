f685eafd72c9a2b410c05d56dc5b0f262a231508
TRIDIP JYOTI DAS <dastridip21@gmail.com>
2019-07-22 16:00:10 +0530

    Update cust.js

--- index/js/cust.js
+++ index/js/cust.js
@@ -1,5 +1,55 @@
-$(document).ready(function(){
-	$('[data-toggle="offcanvas"]').click(function(){
-		$('#side-menu').toggleClass('hidden-xs');
-	});
+$(document).ready(function() {
+  $('[data-toggle="offcanvas"]').click(function() {
+    $("#side-menu").toggleClass("hidden-xs");
+  });
+
+  navigator.getBattery().then(function(battery) {
+    function updateAllBatteryInfo() {
+      updateChargeInfo();
+      updateLevelInfo();
+      updateChargingInfo();
+      updateDischargingInfo();
+    }
+    updateAllBatteryInfo();
+
+    battery.addEventListener("chargingchange", function() {
+      updateChargeInfo();
+    });
+    function updateChargeInfo() {
+      //console.log("Battery charging? " + (battery.charging ? "Yes" : "No"));
+      //   console.log(battery.charging ? "charging" : "Not Charging");
+
+      $("#charge_Status").html(battery.charging ? "Charging" : "Not Charging");
+    }
+
+    battery.addEventListener("levelchange", function() {
+      updateLevelInfo();
+    });
+    function updateLevelInfo() {
+      //   console.log("Battery level: " + battery.level * 100 + "%");
+      //   console.log("Battery level: " + battery.level * 100);
+      //var element = document.getElementById("percent");
+      $("#percent").removeAttr("class");
+      $("#percent").attr("class", "c100 " + "p" + battery.level * 100);
+      $("#percent_txt").html(battery.level * 100 + "%");
+    }
+
+    battery.addEventListener("chargingtimechange", function() {
+      updateChargingInfo();
+    });
+    function updateChargingInfo() {
+      //   console.log(
+      //     "Battery charging time: " + battery.chargingTime + " seconds"
+      //   );
+    }
+
+    battery.addEventListener("dischargingtimechange", function() {
+      updateDischargingInfo();
+    });
+    function updateDischargingInfo() {
+      //   console.log(
+      //     "Battery discharging time: " + battery.dischargingTime + " seconds"
+      //   );
+    }
+  });
 });
